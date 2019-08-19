26d4b13ff6a6789df0712769216b49f6e3735b84
Acquila Santos Rocha <acquila.santos@gmail.com>
2019-08-19 17:25:37 -0300

    first

---
+++ gera-string.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+STRING=""
+for I in $*; do
+   TYPE=$(echo $I | cut -c -1)
+   if [ "$TYPE" == "R" ]; then
+        FILTRO=".restrictions.$I"
+        RESTRICTION=$(cat resultados-busca.json | jq "$FILTRO" | sed 's/,/ /g' | sed 's/\[//g' | sed 's/\]//g' | sed 's/^"//g' | sed 's/"$//g')
+        if [ "$STRING" == "" ]; then
+                STRING="$RESTRICTION"
+        else
+                STRING="$STRING $RESTRICTION"            
+        fi
+   else
+        FILTRO=".keys.$I"
+        KEY=$(cat resultados-busca.json | jq "$FILTRO" | sed 's/,/ OR/g' | sed 's/\[//g' | sed 's/\]//g' )
+        if [ "$STRING" == "" ]; then
+                STRING="($KEY)"
+            else
+                STRING="$STRING AND ($KEY)"            
+        fi
+   fi
+done
+
+echo $STRING
+echo $STRING | xclip -selection c
